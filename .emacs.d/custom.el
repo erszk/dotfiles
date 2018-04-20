1e366595610d68c7a1fadae23e3b0f196a2f8b97
Louis Thompson <erszk.coding@gmail.com>
2018-04-20 16:39:38 -0700

    emacs font; add gitconfig

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -29,6 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "gray80" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo"))))
  '(Man-overstrike ((t (:inherit bold :foreground "dark orange"))))
  '(Man-underline ((t (:inherit bold :foreground "SteelBlue3"))))
  '(hl-line ((t (:background "gray32" :underline "Yellow"))))
