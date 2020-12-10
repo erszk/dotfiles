1fbf164e24a1b9f9cc5ae996c1e21b01256ecaeb
Louis Thompson <erszk.coding@gmail.com>
2020-12-10 13:42:51 -0800

    custom update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,39 +9,36 @@
  ;; If there is more than one, they won't work right.
 	'(ansi-color-names-vector
 		 ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-enabled-themes '(deeper-blue))
 	'(dired-garbage-files-regexp
 		 "\\.\\(?:aux\\|bak\\|dvi\\|log\\|orig\\|rej\\|toc\\|DS_Store\\)\\'")
 	'(flymake-allowed-file-name-masks
-		 (quote
-			 (("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init nil nil)
-				 ("\\.cs\\'" flymake-simple-make-init nil nil)
-				 ("\\.p[ml]\\'" flymake-perl-init nil nil)
-				 ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup nil))))
+		 '(("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init nil nil)
+				("\\.cs\\'" flymake-simple-make-init nil nil)
+				("\\.p[ml]\\'" flymake-perl-init nil nil)
+				("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup nil)))
 	'(flymake-proc-allowed-file-name-masks
-		 (quote
-			 (("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init nil nil)
-				 ("\\.cs\\'" flymake-simple-make-init nil nil)
-				 ("\\.p[ml]\\'" flymake-perl-init nil nil)
-				 ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup nil))))
- '(indicate-buffer-boundaries (quote left))
- '(org-agenda-files (quote ("~/gdrive/chaffey/s19.org")))
+		 '(("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init nil nil)
+				("\\.cs\\'" flymake-simple-make-init nil nil)
+				("\\.p[ml]\\'" flymake-perl-init nil nil)
+				("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup nil)))
+ '(indicate-buffer-boundaries 'left)
+ '(org-agenda-files '("~/gdrive/chaffey/s19.org"))
 	'(package-selected-packages
-		 (quote
-			 (web-mode emmet emmet-mode rubik graphviz-dot-mode forth-mode hy-mode editorconfig lua-mode fountain-mode haskell-mode abc-mode yaml-mode csv-mode yasnippet aggressive-indent ace-link company agressive-indent markdown-mode expand-region which-key paradox use-package typit magit paredit slime)))
+		 '(json-mode web-mode emmet emmet-mode rubik graphviz-dot-mode forth-mode hy-mode editorconfig lua-mode fountain-mode haskell-mode abc-mode yaml-mode csv-mode yasnippet aggressive-indent ace-link company agressive-indent markdown-mode expand-region which-key paradox use-package typit magit paredit slime))
  '(paradox-github-token t))
 
 (custom-set-faces
-	;; custom-set-faces was added by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
-	'(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "gray80" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo"))))
-	'(Man-overstrike ((t (:inherit bold :foreground "dark orange"))))
-	'(Man-underline ((t (:inherit bold :foreground "SteelBlue3"))))
-	'(hl-line ((t (:background "gray32" :underline "Yellow"))))
-	'(mode-line ((t (:box 1))))
-	'(region ((t (:background "firebrick4" :foreground "#f6f3e8")))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "gray80" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo"))))
+ '(Man-overstrike ((t (:inherit bold :foreground "dark orange"))))
+ '(Man-underline ((t (:inherit bold :foreground "SteelBlue3"))))
+ '(hl-line ((t (:background "gray32" :underline "Yellow"))))
+ '(mode-line ((t (:box 1))))
+ '(region ((t (:background "firebrick4" :foreground "#f6f3e8")))))
 
 
 ;;; EXTRACTED FROM CUSTOMIZE
